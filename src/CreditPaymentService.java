298a651929c6c31176a1c4de4723e4b140f3291a
NetilogyIvan <inkornelyuk@gmail.com>
2023-03-22 19:49:17 +0600

    Reformat code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,7 +2,7 @@ public class CreditPaymentService {
 
     public int calculate(double interestRate, double amountOfCredit, int loanTerms) {
         double resultOne = amountOfCredit * (interestRate / (12 * 100));
-        double resultTwo = 1 - Math.pow (1 + (interestRate / (12 * 100)), -loanTerms);
+        double resultTwo = 1 - Math.pow(1 + (interestRate / (12 * 100)), -loanTerms);
         double monthlyPayment = resultOne / resultTwo;
 
         return (int) monthlyPayment;
